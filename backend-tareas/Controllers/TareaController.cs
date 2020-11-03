be099f238c1ae4a78a6c4976e6124ca4044c13b3
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-03 11:46:12 -0300

    Creacion controllador y get tareas

---
+++ backend-tareas/Controllers/TareaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend_tareas.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_tareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public TareaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listTareas = await _context.Tareas.ToListAsync();
+                return Ok(listTareas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
